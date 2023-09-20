439d74f2274576d83041b0100a2e4cfa3aba1242
GiacomoGiovannetti <giovannettii.giacomo@gmail.com>
2023-09-20 17:02:39 +0200

    creazione preliminare delle pagine guided meditation e first steps

--- src/pages/FirstSteps.jsx
+++ src/pages/FirstSteps.jsx
@@ -1,7 +1,70 @@
+import { useState } from "react";
+import { FaCaretDown, FaCaretUp } from "react-icons/fa6";
+
+const questions = [
+  "what is meditation",
+  "which are the benefits of meditating",
+  "how to get started",
+  "which are the different types of meditation",
+  "what should i do if my mind wanders during meditation",
+];
+
 export const FirstSteps = () => {
+  const [isOpen, setIsOpen] = useState({
+    whatIs: false,
+    whyStartPracticing: false,
+    startDoing: false,
+  });
+
+  const handleClick = (e) => {
+    let section = e.target.id;
+    console.log(section);
+
+    switch (section) {
+      case "what-is":
+        setIsOpen((prev) => ({ ...prev, whatIs: !prev.whatIs }));
+        break;
+      case "why-start":
+        setIsOpen((prev) => ({
+          ...prev,
+          whyStartPracticing: !prev.whyStartPracticing,
+        }));
+        break;
+      case "start-doing":
+        setIsOpen((prev) => ({ ...prev, startDoing: !prev.startDoing }));
+    }
+  };
   return (
-    <div>
+    <div onClick={handleClick}>
       <h1>Prova First Steps</h1>
+
+      <div className="flex flex-row items-center relative">
+        <h3 id="what-is">What Is Meditation </h3>
+        {isOpen.whatIs ? <FaCaretUp /> : <FaCaretDown />}
+      </div>
+
+      <div className="flex flex-col items-center ">
+        <h3 id="why-start">Why you should start practicing it</h3>
+        {isOpen.whyStartPracticing ? <FaCaretUp /> : <FaCaretDown />}
+
+        {isOpen.whyStartPracticing && (
+          <div className="">
+            <p>
+              Lorem ipsum dolor sit amet. Nam praesentium molestias sit odit
+              placeat sed vitae dignissimos ut repudiandae nesciunt? Qui
+              delectus repudiandae a distinctio sint ea nesciunt eveniet sit
+              obcaecati blanditiis in ipsum accusantium aut quia similique. Qui
+              atque accusantium eos dolores ducimus vel nobis alias ea dolores
+              doloribus non error fugiat et enim deleniti.
+            </p>
+          </div>
+        )}
+      </div>
+
+      <div className="flex flex-row items-center relative">
+        <h3 id="start-doing">How to start Meditating</h3>
+        {isOpen.startDoing ? <FaCaretUp /> : <FaCaretDown />}
+      </div>
     </div>
   );
 };
--- src/pages/GuidedMeditation.jsx
+++ src/pages/GuidedMeditation.jsx
@@ -1,11 +1,45 @@
+import { useState } from "react";
+import { FaCaretDown, FaCaretUp } from "react-icons/fa6";
 import { Soundtracks } from "../components/Soundtracks";
 import { Timer } from "../components/Timer";
 
 export const GuidedMeditations = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedMeditation, setSelectedMeditation] = useState("");
+
+  const closeSelection = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const handleClick = (e) => {
+    let meditation = e.target.innerText;
+    setSelectedMeditation(meditation);
+  };
+
   return (
-    <div>
-      <div className="select-meditation-dropdown">
-        <h1>Select guided Meditation </h1>
+    <div className="flex flex-col items-center">
+      <div
+        className="select-meditation-dropdown flex flex-row items-center relative"
+        onClick={closeSelection}
+      >
+        <h1>
+          {selectedMeditation === ""
+            ? "Select guided Meditation"
+            : selectedMeditation}
+        </h1>
+        {isOpen ? <FaCaretUp /> : <FaCaretDown />}
+        {isOpen && (
+          <div
+            className="menu absolute top-8 right-0 z-10 bg-amber-400 rounded-xl"
+            onClick={handleClick}
+          >
+            <p>Gratitude Meditation --- 10 Minutes</p>
+            <p>Breath awareness Meditation --- 10 Minutes</p>
+            <p>Sleep Meditation --- 10 Minutes</p>
+            <p>Sensation recognition Meditation --- 10 Minutes</p>
+            <p>Law of Attraction meditation --- 10 Minutes</p>
+          </div>
+        )}
       </div>
 
       <Timer />
