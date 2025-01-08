4df200152635110a770ca9fbc272e7caa6be7e6a
Giacomo Giovannetti <giovannettii.giacomo@gmail.com>
2025-01-08 15:33:18 +0100

    migrated to helmet static instead of async

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import ReactGA from 'react-ga4';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { Helmet } from 'react-helmet';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 import { Footer } from './components/Footer';
@@ -17,7 +17,6 @@ import { TimerProvider } from './context/timerContext';
 
 export default function App() {
   const [darkMode, setDarkMode] = useState(false);
-  const helmetContext = {};
 
   ReactGA.initialize('G-X30TL20ZZS');
 
@@ -29,8 +28,8 @@ export default function App() {
     <PathProvider>
       <DataProvider>
         <TimerProvider>
-          <HelmetProvider context={helmetContext}>
-            <Router>
+          <Router>
+            <div className={`${darkMode ? 'dark' : ''}`}>
               <Helmet>
                 <title>Awaken Mind</title>
                 <meta property='og:title' content='Awaken Mind' />
@@ -43,30 +42,28 @@ export default function App() {
                 <meta
                   property='og:description'
                   content='Awaken Mind is a Web App used to
-                  practice meditation by setting up a custom timer or by following a guided
-                  meditation.'
+                practice meditation by setting up a custom timer or by following a guided
+                meditation.'
                 />
               </Helmet>
-              <div className={`${darkMode ? 'dark' : ''}`}>
-                <main className='flex flex-col font-Mooli h-auto min-h-screen text-slate-800 bg-gradient-to-b from-amber-500  via-yellow-400 to-blue-300 dark:from-slate-900 dark:via-slate-900  dark:to-slate-800 dark:text-amber-500'>
-                  <NavBar darkMode={darkMode} setDarkMode={setDarkMode} />
-                  <Routes>
-                    <Route exact path='/' element={<Home />}></Route>
-                    <Route
-                      path='/custom-meditations'
-                      element={<CustomMeditations darkMode={darkMode} />}
-                    ></Route>
-                    <Route
-                      path='/guided-meditations'
-                      element={<GuidedMeditations darkMode={darkMode} />}
-                    ></Route>
-                    <Route path='/first-steps' element={<FirstSteps />}></Route>
-                  </Routes>
-                  <Footer />
-                </main>
-              </div>
-            </Router>
-          </HelmetProvider>
+              <main className='flex flex-col font-Mooli h-auto min-h-screen text-slate-800 bg-gradient-to-b from-amber-500  via-yellow-400 to-blue-300 dark:from-slate-900 dark:via-slate-900  dark:to-slate-800 dark:text-amber-500'>
+                <NavBar darkMode={darkMode} setDarkMode={setDarkMode} />
+                <Routes>
+                  <Route exact path='/' element={<Home />}></Route>
+                  <Route
+                    path='/custom-meditations'
+                    element={<CustomMeditations darkMode={darkMode} />}
+                  ></Route>
+                  <Route
+                    path='/guided-meditations'
+                    element={<GuidedMeditations darkMode={darkMode} />}
+                  ></Route>
+                  <Route path='/first-steps' element={<FirstSteps />}></Route>
+                </Routes>
+                <Footer />
+              </main>
+            </div>
+          </Router>
         </TimerProvider>
       </DataProvider>
     </PathProvider>
